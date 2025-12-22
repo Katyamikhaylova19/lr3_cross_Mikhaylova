3ea886e46310253e2b762f7ff127aced630cf091
Katyamikhaylova19 <megalogin32015@gmail.com>
2025-12-22 21:26:37 +0300

    Rework project for more agile

--- teacher-rating-ui/src/app/models/group.model.ts
+++
@@ -1,9 +0,0 @@
-import { Student } from "./student.model";
-import { TeacherGroup } from "./teacher.model";
-
-export interface Group {
-  id: number;
-  groupNumber: string;
-  students?: Student[];
-  teacherGroups?: TeacherGroup[];
-}
